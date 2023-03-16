83cb2b0edb6e117fd9f12839505ce09b011d20e1
Bruno Maia <111490358+brunomaiaa@users.noreply.github.com>
2023-03-16 13:18:41 +0100

    Creation of the function to edit projects
    Creation of the function to edit projects

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -1,7 +1,121 @@
 import React from 'react'
+import styles from './Project.module.css'
+import {useParams} from 'react-router-dom'
+import {useEffect, useState} from 'react'
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+import ProjectForm from '../project/ProjectForm'
+import Message from '../layout/Message'
+
 
 export default function Project() {
+
+const {id} = useParams()
+
+const [project, setProject] = useState([])
+
+const [showProjectForm, setShowProjectForm] = useState(false)
+
+const [message, setMessage] = useState()
+const [type, setType] = useState()
+
+
+useEffect(() => {
+  setTimeout(() => { 
+    fetch(`http://localhost:5000/projects/${id}`,
+    { method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+     }
+    )
+    .then((res) => res.json())
+    .then((data) => setProject(data))
+    .catch((err) => console.log(err))
+
+  }, 300)
+
+}, [id])
+
+function editPost (project) {
+  //budget validation
+  if (project.budget < project.costs) {
+   setMessage('Budget must be higher than total used')
+    setType('error')
+    return false
+  }
+
+
+
+
+  fetch(`http://localhost:5000/projects/${project.id}`,
+  { method: 'PUT',
+  headers: {
+      'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(project),
+   }
+  )
+  .then((res) => res.json())
+  .then((data) => {
+    setProject(data)
+    setShowProjectForm(false)
+    //mensagem
+    
+      setMessage('Project updated')
+       setType('success')
+      
+     })
+  
+  .catch((err) => console.log(err))
+}
+
+const toggleProjectForm = () => {
+  setShowProjectForm(!showProjectForm)
+
+}
+
+
+
   return (
-    <div>Project</div>
+    <>
+      {project.name ? (
+       <div className={styles.project_details}>
+        <Container customClass="column" >
+          {message && <Message type={type} msg={message} />}
+          <div className={styles.details_container}>
+            <h1>Project:{project.name}</h1>
+            <button className={styles.btn} onClick={toggleProjectForm}>
+              {!showProjectForm? 'Edit project' : 'Cancel'}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                 <p>
+                  <span>Category:</span>{project.category.name}
+                 </p>
+                  <p>
+                    <span>Budget:</span>€{project.budget}
+                  </p>
+                  <p>
+                    <span>Total used:</span>€{project.costs}
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                 <ProjectForm handleSubmit={editPost} btnText="Complete" projectData={project}/>
+                </div>
+              )
+              }
+          </div>
+
+        </Container>
+       </div>
+       )
+       : 
+       (
+       <Loading/>
+       )
+       }
+    </>
   )
 }
--- costs/src/components/project/ProjectCard.js
+++ costs/src/components/project/ProjectCard.js
@@ -27,7 +27,7 @@ export default function ProjectCard({id, name, budget, category, handleRemove}) 
             
         </p>
         <div className={styles.project_card_actions}>
-            <Link to="/">
+            <Link to={`/project/${id}`}>
                 <BsPencil /> Edit
             </Link>
             <button onClick={remove}>
